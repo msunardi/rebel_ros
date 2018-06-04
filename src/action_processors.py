5d6bec7ac1e72a9d3cb2751f8e6912b21cc87d13
Mathias Sunardi <misunardi@hotmail.com>
2018-06-04 14:00:09 -0700

    Factor out merge function

---
+++ src/action_processors.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from scipy.interpolate import interp1d
+import scipy.fftpack as spfft
+import matplotlib.pyplot as plt
+
+from utils import elapsed, rprint
+
+@elapsed
+def merges(*data, **kwargs):
+    '''Accepts arbitrary number of data points to merge
+       Merges using FFT
+       Returns: inverse FFT of merged signals
+    '''
+    
+    joint='R_SHO_PITCH'
+    if 'joint' in kwargs:
+        joint = kwargs['joint']
+    N=400
+    if 'N' in kwargs:
+        N = kwargs['N']
+    p=[1.0]
+    if 'p' in kwargs:
+        p = kwargs['p']
+    
+    assert len(p) == len(data), "[MERGE2]: Mismatch number of values in p to perform weighted sum! p size={}, data size={}".format(len(p), len(data))
+    assert sum(p) == 1.0, "[MERGE2]: P value does not sum to 1: {}".format(p)
+    
+    LL = len(data)
+    if LL == 1:
+        print("[MERGE2]: There's only one data. Returning as-is.")
+        return data
+    
+    # Find 
+    just_joint_data = [d[joint] for d in data]
+    data_lengths = [len(dx) for dx in just_joint_data]
+    max_data_lengths = max(data_lengths)
+    
+    # Container for all Fourier'd signals
+    fftx_all = []
+    all_data = []
+    
+    T = 1.0/800.0
+    xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
+    for dd in just_joint_data:
+        l = len(dd)
+        
+        x = np.arange(0, l)
+        
+        fx = interp1d(x, dd, kind='cubic', fill_value='extrapolate')
+        
+        xnew = np.linspace(0, l, N*2, endpoint=True)        
+        
+        fxxnew = fx(xnew)[:-50]
+        fftx = spfft.fft(fxxnew)
+        fftx_all.append((xf, fftx))
+#        print(len(xnew), len(dd))
+        all_data.append((xnew, fxxnew))
+        
+    # Combined data
+    le_p = np.array(p)
+    fftx_all_array = np.array(fftx_all)[:,1]
+    assert le_p.shape == fftx_all_array.shape, "[MERGE2]: le_p shape: {}, fftx_shape: {}".format(le_p.shape, fftx_all_array.shape)
+        
+    fft_combined = np.dot(le_p, fftx_all_array)
+    print("[MERGE2]: fft_combined.shape: {}".format(fft_combined.shape))
+
+    ifft_combined = spfft.ifft(fft_combined)
+    fftx_all.append((xf, fft_combined))
+    ix = np.linspace(0, max_data_lengths, ifft_combined.shape[0])
+    
+    all_data.append((ix, ifft_combined))
+    
+    f, ax = plt.subplots(len(all_data), 2, figsize=(8,7))
+    
+    assert len(all_data) == len(fftx_all)
+    
+    print("[MERGE2]: Will plot {} data.".format(len(all_data)))
+    
+    for j in np.arange(len(all_data)):
+        xnew, fxxnew = all_data[j]
+        if len(xnew) != len(fxxnew):
+            ll = min(len(xnew), len(fxxnew))
+        ax[j, 0].plot(xnew[:ll], fxxnew[:ll])   
+        
+#        assert len(xnew) == len(fxxnew), "xnew: {} vs. fxxnew: {}".format(len(xnew), len(fxxnew))
+#        ax[j, 0].plot(xnew, fxxnew)
+    
+    for i in np.arange(len(fftx_all)):
+        xff, fftxx = fftx_all[i]
+        ax[i, 1].plot(xff, 2.0/N*abs(fftxx.imag[:N//2]))
+        
+    
+    plt.show()
+    return ifft_combined
